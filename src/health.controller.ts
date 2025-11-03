b8f20ed036435f2cde1cc90b0424f13ee4d62331
Geometry <hellogeometrysoftware@gmail.com>
2025-11-03 17:46:17 -0300

    HealthController

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { Connection } from 'mongoose';
+import { InjectConnection } from '@nestjs/mongoose';
 
-@Controller()
+@Controller('health')
 export class HealthController {
-  @Get('health')
-  health() {
-    return { ok: true };
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  @Get()
+  getHealth() {
+    const isConnected = this.connection.readyState === 1; // 1 = connected
+    return { status: isConnected ? 'ok' : 'error' };
   }
 }
