15286f7cfbf58ed7a4e1a5c22802b3404266587a
Geometry <hellogeometrysoftware@gmail.com>
2025-11-03 17:21:35 -0300

    docker

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -5,7 +5,7 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true, unique: true, trim: true, lowercase: true })
+  @Prop({ required: true, unique: true })
   email: string;
 
   @Prop({ required: true, select: false })
@@ -13,4 +13,3 @@ export class User {
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
-UserSchema.index({ email: 1 }, { unique: true });
